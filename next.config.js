c9979e7f0ec2b82b9a7e32b29e81f65aa0b18800
HKUSTSMARTLab <petergamsing@gmail.com>
2025-03-25 19:08:44 +0800

    Update config with static deployment settings

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -21,3 +10,20 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   // 设置基本路径为 /showcase/PathBench
+//   basePath: '/showcase/PathBench',
+//   // 资源前缀也设置为相同路径
+//   assetPrefix: '/showcase/PathBench',
+//   // 确保图像和其他资源正确加载
+//   images: {
+//     unoptimized: true,
+//   },
+//   // 如果您的应用使用了 trailing slashes
+//   trailingSlash: true,
+// }
+
+// module.exports = nextConfig
