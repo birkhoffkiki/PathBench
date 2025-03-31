721edba1fa8cc0c90b7fdb87237b6cdb15918889
HKUSTSMARTLab <petergamsing@gmail.com>
2025-03-31 16:48:44 +0800

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,
-//   },
-
-//   // 自动根据环境设置prefix
-//   basePath: '/PathBench',
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/showcase/PathBench',
-  assetPrefix: '/showcase/PathBench',
+  basePath: '/PathBench',
+  assetPrefix: '/PathBench',
   images: {
     unoptimized: true,
   },
@@ -30,3 +17,23 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/showcase/PathBench',
+//   assetPrefix: '/showcase/PathBench',
+//   images: {
+//     unoptimized: true,
+//   },
+
+//   trailingSlash: true,
+//   env: {
+//     // These values will be overwritten by the build process
+//     NEXT_PUBLIC_BUILD_DATE: new Date().toISOString().split('T')[0],
+//     NEXT_PUBLIC_BUILD_TIME: new Date().toISOString().split('T')[1].substring(0, 8),
+//     NEXT_PUBLIC_BUILD_TIMESTAMP: new Date().toISOString().replace('T', ' ').substring(0, 19)
+//   }
+// }
+
+// module.exports = nextConfig
